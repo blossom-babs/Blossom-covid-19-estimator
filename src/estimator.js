4e107c720aa564e4c37000614c520ff0cdacf65f
Blossom-Babs <saphashb@gmail.com>
2020-04-13 03:11:13 +0100

    update

--- src/estimator.js
+++ src/estimator.js
@@ -3,26 +3,25 @@ const covid19ImpactEstimator = (data) => {
 
   const timeElapsed = (periodType, timeToElapse) => {
     if (periodType === 'days') {
-      return Math.trunc(timeToElapse / 3);
+      return timeToElapse / 3;
     }
     if (periodType === 'weeks') {
-      return Math.trunc((timeToElapse * 7) / 3);
+      return timeToElapse * 7;
     }
     if (periodType === 'months') {
-      return Math.trunc((timeToElapse * 30) / 3);
+      return timeToElapse * 30;
     }
     return null;
   };
 
-
-  const income = Math.trunc(0.65 * (data.population / data.avgDailyIncomePopulation));
+  const factor = Math.trunc(timeElapsed(data.periodType, data.timeToElapse) / 3);
+  const income = (data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD);
   const currentlyInfected = data.reportedCases * 10;
 
-  const infectionsByRequestedTime = currentlyInfected * (2 ** (timeElapsed('days', 38)));
+  const infectionsByRequestedTime = currentlyInfected * (2 ** (factor));
   const severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
   const availableBeds = 0.35 * data.totalHospitalBeds;
 
-  const estimate = [];
 
   const impact = {
     currentlyInfected,
@@ -31,12 +30,12 @@ const covid19ImpactEstimator = (data) => {
     hospitalBedsByRequestedTime: data.totalHospitalBeds - severeCasesByRequestedTime,
     casesForICUByRequestedTime: Math.trunc(availableBeds * infectionsByRequestedTime),
     casesForVentilatorsByRequestedTime: Math.trunc(0.02 * infectionsByRequestedTime),
-    dollarsInFight: (infectionsByRequestedTime * income * data.avgDailyIncomeInUSD) / 30
+    dollarsInFight: (infectionsByRequestedTime * income) / factor
 
   };
 
   const siCurrentlyInfected = data.reportedCases * 50;
-  const siInfectionsByRequestedTime = siCurrentlyInfected * (2 ** (timeElapsed('days', 38)));
+  const siInfectionsByRequestedTime = siCurrentlyInfected * (2 ** (factor));
   const siSevereCasesByRequestedTime = 0.15 * siInfectionsByRequestedTime;
 
   const severeImpact = {
@@ -46,10 +45,8 @@ const covid19ImpactEstimator = (data) => {
     hospitalBedsByRequestedTime: Math.trunc(availableBeds - siSevereCasesByRequestedTime),
     casesForICUByRequestedTime: 0.05 * siInfectionsByRequestedTime,
     casesForVentilatorsByRequestedTime: 0.02 * siInfectionsByRequestedTime,
-    dollarsInFight: (siInfectionsByRequestedTime * income * data.avgDailyIncomeInUSD) / 30
+    dollarsInFight: (siInfectionsByRequestedTime * income) / factor
   };
-
-  estimate.push({ impact, severeImpact });
 
   return {
     data: JSON.stringify(input),
